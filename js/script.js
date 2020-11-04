24077f0f43d18277c132cd17efa703135379284f
Mattia <mattia.tummolilllo@gmail.com>
2020-11-04 17:06:21 +0100

    Completamento esercizio (tranne il controllo)

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
 
 
-   $(".dropitem>a").click(function () {
-      $(".dropdown").each(function () {
-         $(this).toggleClass("dBlock");
-      });
+   $(".dropitem").click(function () {
+      $(this).find(".dropdown").toggleClass("dBlock");
    });
-
-
 
 
 
